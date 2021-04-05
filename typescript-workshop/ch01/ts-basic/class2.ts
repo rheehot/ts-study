1220503e7063870f50fca85ede5382635c3cfe1e
WonyJeong <59948675+WonyJeong@users.noreply.github.com>
2021-04-05 23:52:17 +0900

    ch01 - ts기초

--- typescript-workshop/ch01/ts-basic/class2.ts
+++ typescript-workshop/ch01/ts-basic/class2.ts
@@ -7,20 +7,34 @@ interface Programer {
     writeCode(requirment : string) : string
 }
 
-class KoreanProgramer implements Person, Programer{
-    constructor(public name : string) {
+abstract class Korean implements Person{
+    public abstract jumin : number
+    
+    constructor(public name : string){}
+
+    say(message: string): void {
+        console.log(message)
+    }
+
+    abstract loveKimchi() : void   
+}
+
+class KoreanProgramer extends Korean implements Programer{
+    constructor(public name : string = "wony", public jumin : number = 95018) {
+        //parent constructor call
+        super(name)
     }
     
     say(message : string): void{
         console.log(`Hi ! ${message}`)
     }
 
-    writeCode(requirment : string) {
+    writeCode(requirment : string) : string {
         console.log(requirment)
         return requirment + "....."
     }
 
-    loveKimchi(){
+    loveKimchi() : void {
         console.log("Love~ Kimchi")
     }
 }
